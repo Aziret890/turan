f4f2185cb8aac7fcf915121854721f8547931ede
aziret <kubaaziret68@gmail.com>
2024-03-22 17:41:32 +0600

    aziret finish scroll

--- src/components/accessory/Accessory.tsx
+++ src/components/accessory/Accessory.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import AccessoryChild from "./AccessoryChild";
+import "./Accessory.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 function Accessory() {
   return (
-    <div>Accessory</div>
-  )
+    <section>
+      <div className="container">
+        <div className="accessory__content mt-14">
+          <h1 className="mb-6">Аксессуары</h1>
+          <div className="flex justify-between gap-[30px] accessory__coontent-slider">
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+            <AccessoryChild />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Accessory
+export default Accessory;

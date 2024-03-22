825fd30c7b9b341f9b70c09f21c5199e6864fe2a
aziret <kubaaziret68@gmail.com>
2024-03-22 15:50:29 +0600

    finish brandRout

--- src/components/slider/routesBrand/Index.tsx
+++ src/components/slider/routesBrand/Index.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import "./style.scss";
+import apple from "../../../shared/images/appleHero.png";
+import xiaomi from "../../../shared/images/xiaomiHero.png";
+import sony from "../../../shared/images/soniHero.png";
+import dyson from "../../../shared/images/dysonHero.png";
 
-function Index() {
+function BrandRout() {
   return (
-    <div>Index</div>
-  )
+    <div className="container">
+      <div className="brandRout__content mt-14">
+        <div className="brandRout__content__left">
+          <img src={dyson} alt="" />
+          <button>dyson</button>
+        </div>
+        <div className="brandRout__content__right ">
+          <div className="brandRout__content__right-child">
+            <img src={apple} alt="" />
+            <button>apple</button>
+          </div>
+          <div className="brandRout__content__right-child">
+            <img src={xiaomi} alt="" />
+            <button>xiaomi</button>
+          </div>
+          <div className="brandRout__content__right-child">
+            <img src={sony} alt="" />
+            <button>Sony Playstation</button>
+          </div>
+          <div className="brandRout__content__right-child">
+            <img src={sony} alt="" />
+            <button>Sony Playstation</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Index
+export default BrandRout;

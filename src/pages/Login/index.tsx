9beaa9ee239e345d8e8b8e170d840930e494977c
aziret <kubaaziret68@gmail.com>
2024-03-25 17:23:44 +0600

    finish add registration

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,45 @@
 import "./styles.scss";
+import registrationElement from "../../shared/images/registrationElement.png";
+import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <section>
       <div className="container">
         <h1>Авторизация</h1>
-        <div className="login__content">
-          <div className="w-[80%]">
-            <p className="text-center">
+        {/* <div className="login__content flex flex-col gap-[20px] items-center">
+          <div className="w-[80%] flex justify-center">
+            <p className="text-center w-[70%]">
               Авторизуйтесь, указав свои контактные данные, или воспользовавшись
               перечисленными сервисами
             </p>
           </div>
-          <input type="text" name="" id="" placeholder="" />
-          <input type="text" name="" id="" placeholder="" />
-          <div>
-            <button>Войти</button>
+          <div className="flex flex-col items-center gap-[20px]">
+            <input type="text" name="" id="" placeholder="Имя" />
+            <input type="text" name="" id="" placeholder="Пароль" />
+            <input type="text" name="" id="" placeholder="Потвердить Пароль" />
+            <input type="text" name="" id="" placeholder="Email" />
+            <div className="flex gap-[20px]">
+              <input type="checkbox" className="login__content-checkbox" />
+              <p>
+                <span>
+                  Я согласен с условиями обработки персональных данных
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
+          <div>
+            <button>Регистрация</button>
+          </div>
+          <img src={registrationElement} alt="" />
+          <div className="flex gap-[30px] items-center">
+            <p>
+              <span>Уже регистрировались?</span>{" "}
+            </p>
+            <Link to={"/"}>
+              <h4>Войти</h4>
+            </Link>
+          </div>
+        </div> */}
       </div>
     </section>
   );
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,3 +1,53 @@
-export default function Registration() {
-	return <div>Registration</div>
+import "./styles.scss";
+import registrationElement from "../../shared/images/registrationElement.png";
+import { Link } from "react-router-dom";
+export default function Login() {
+  return (
+    <section>
+      <div className="container">
+        <h1>Авторизация</h1>
+        <div className="flex justify-center mt-[50px]">
+          <div className="login__content flex flex-col gap-[20px] items-center">
+            <div className="w-[80%] flex justify-center">
+              <p className="text-center w-[70%]">
+                Авторизуйтесь, указав свои контактные данные, или
+                воспользовавшись перечисленными сервисами
+              </p>
+            </div>
+            <div className="flex flex-col items-center gap-[20px]">
+              <input type="text" name="" id="" placeholder="Имя" />
+              <input type="text" name="" id="" placeholder="Пароль" />
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Потвердить Пароль"
+              />
+              <input type="text" name="" id="" placeholder="Email" />
+              <div className="flex gap-[20px]">
+                <input type="checkbox" className="login__content-checkbox" />
+                <p>
+                  <span>
+                    Я согласен с условиями обработки персональных данных
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div>
+              <button>Регистрация</button>
+            </div>
+            <img src={registrationElement} alt="" />
+            <div className="flex gap-[30px] items-center">
+              <p>
+                <span>Уже регистрировались?</span>{" "}
+              </p>
+              <Link to={"/"}>
+                <h4>Войти</h4>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }

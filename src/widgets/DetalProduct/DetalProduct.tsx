870c6cf6a1a1a1b45b3d494fad722c077a793d13
aziret <kubaaziret68@gmail.com>
2024-03-27 16:12:34 +0600

    finish detal page bekend

--- src/widgets/DetalProduct/DetalProduct.tsx
+++ src/widgets/DetalProduct/DetalProduct.tsx
@@ -1,57 +1,64 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.scss";
 import phone from "../../shared/images/apple14.png";
 import star from "../../shared/images/starAll.png";
 import love from "../../shared/svg/love.svg";
+import axios from "axios";
 
-// interface DataInfo {
-//   images: string[];
-//   title: string;
-//   price: number;
-//   guarantee: string;
-//   screen: string;
-//   screenTechnology: string;
-//   numberOfCores: string;
-//   infoProduct: string;
-//   color?: string;
-// }
+interface DataInfo {
+  images: string[];
+  name: string;
+  price: number;
+  description: string;
+  model: object[];
+  characteristics: object[];
+}
 function DetalProduct() {
+  const [valueImage, setValueImage] = useState<number>(0);
   const [active, setActive] = useState<string>("");
-  // const dataInfo: DataInfo = {
-  //   images: [phone, phone, phone],
-  //   title: "Apple IPhone 14 Pro Max ",
-  //   price: 75400,
-  //   guarantee: "",
-  //   screen: "",
-  //   screenTechnology: "",
-  //   numberOfCores: "",
-  //   infoProduct:
-  //     "Смартфон iPhone 14 в корпусе цвета Gold со встроенной памятью 128 Гб оснащен экраном диагональю 6,1 дюйма, выполненным по технологии OLED. Дисплей типа Super Retina XDR обладает разрешением 2532x1170 пикселей. В этой модели установлен шестиядерный процессор А15 Bionic. В девайсе ",
-  // };
+  const [data, setData] = useState<object>({});
+  useEffect(() => {
+    axios
+      .get<DataInfo>("https://takmatov.pythonanywhere.com/product/15/")
+      .then((res) => setData(res.data));
+  }, []);
+  console.log(data);
   return (
     <section className="detalProduct">
       <div className="container">
-        <h2>Главная / Каталог / Iphone / {"Ipnone 14 Pro max"}</h2>
+        <h2>Главная / Каталог / Iphone / {data.name}</h2>
         <div className="detalProduct__content flex justify-between">
           <div className="detalProduct__content__left">
             <div className="detalProduct__content__left-top flex justify-center items-center">
               <div className="w-[420px] h-[335px]">
-                <img
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "contain",
-                  }}
-                  src={phone}
-                  alt=""
-                />
+                {data.images && data.images.length > 0 && (
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
+                    src={`https://takmatov.pythonanywhere.com/${data.images[valueImage]}`}
+                    alt=""
+                  />
+                )}
               </div>
             </div>
             <div className="detalProduct__content__left-top-wrapper flex justify-between mt-[45px]">
+              {data.images?.map((el, inx) => (
+                <div
+                  onClick={() => setValueImage(inx)}
+                  className="detalProduct__content__left-child"
+                >
+                  <img
+                    src={`https://takmatov.pythonanywhere.com/${el}`}
+                    alt=""
+                  />
+                </div>
+              ))}
+              {/* <div className="detalProduct__content__left-child"></div>
               <div className="detalProduct__content__left-child"></div>
-              <div className="detalProduct__content__left-child"></div>
-              <div className="detalProduct__content__left-child"></div>
-              <div className="detalProduct__content__left-child"></div>
+              <div className="detalProduct__content__left-child"></div> */}
             </div>
             <div className="flex justify-start items-center mt-4 gap-[50px]">
               <h2>Выбрать цвет</h2>
@@ -59,14 +66,14 @@ function DetalProduct() {
             </div>
             <div className="detalProduct__content__left-review">
               <h1>Описание</h1>
-              <p></p>
+              <p>{data.description}</p>
             </div>
           </div>
           <div className="detalProduct__content__right">
             <div className="flex justify-end">
               <img src={star} alt="" />
             </div>
-            <h1>gege</h1>
+            <h1>{data.name}</h1>
             <div className="flex gap-[30px]">
               <p>Память</p>
               <div className="flex justify-start gap-[30px]">
@@ -78,7 +85,7 @@ function DetalProduct() {
                 </button>
               </div>
             </div>
-            <h1>74500</h1>
+            <h1>{data.price} сом</h1>
             <div className="flex justify-start gap-[40px]">
               <button className="detalProduct__content__right-button-bay">
                 В корзину
@@ -87,7 +94,11 @@ function DetalProduct() {
             </div>
             <h2>Характеристики:</h2>
             <div>
-              <p>vwnm....{"cw"}</p>
+              {data.characteristics?.map((el) => (
+                <p>
+                  {el.title}......{el.value}
+                </p>
+              ))}
             </div>
           </div>
         </div>

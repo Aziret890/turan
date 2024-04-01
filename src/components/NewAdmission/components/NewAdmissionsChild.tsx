57ef4d8d31985edb2805673d67bfea089514b5bc
aziret <kubaaziret68@gmail.com>
2024-04-01 10:38:03 +0600

    aziret

--- src/components/NewAdmission/components/NewAdmissionsChild.tsx
+++ src/components/NewAdmission/components/NewAdmissionsChild.tsx
@@ -13,7 +13,7 @@ interface IProps {
   colors?: string[];
   rating: number;
 }
-
+//vwfgef
 function NewAdmissionsChild({ images, price, colors, id, rating }: IProps) {
   const userJwt = Cookies.get("jwt");
   const [basketId, setBAsketID] = useState(null);
